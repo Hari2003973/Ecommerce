73f84c9e4009ea20c59257be6d899fd8354de6bb
Hari2003973 <svrsrihari@gmail.com>
2025-11-10 15:43:28 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['ecommerce-c0av.onrender.com']
     host: true,
     port: 5173,
     proxy: {
